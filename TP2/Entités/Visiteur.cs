247395f21f0239ddb7fa9a03d8692ce5d9c56b2c
MeuLee <49310962+MeuLee@users.noreply.github.com>
2019-04-17 14:06:20 -0400

    Le nom des visiteurs est random selon une liste

--- TP2/Entités/Visiteur.cs
+++ TP2/Entités/Visiteur.cs
@@ -15,6 +15,16 @@ namespace TP2.Entités
         public DateTime QuandEntreZoo { get; set; }
         public SexeEntite SexeVisiteur { get; set; }
         public string Nom { get; set; }
+        private List<string> _prenoms = new List<string>
+        {
+            "Camille", "Louise", "Ambre", "Agathe", "Jade", "Julia", "Mila", "Alice", "Emma", "Anna", "Lucie", "Eden", "Romane", "Lola", "Emy",
+            "Louis", "Gabriel", "Paul", "Hugo", "Valentin", "Gabin", "Arthur", "Jules", "Lucas", "Sacha", "Ethan", "Antoine", "Nathan", "Thomas", "Tom"
+        };
+
+        private List<string> _noms = new List<string>
+        {
+            "Tremblay", "Gagnon", "Roy", "Bouchard", "Gauthier", "Morin", "Lavoie", "Fortin", "Ouellet", "Pelletier", "Bergeron", "Leblanc", "Paquette", "Girard", "Simard"
+        };
 
         public Visiteur(TuileZoo position)
         {
@@ -22,6 +32,29 @@ namespace TP2.Entités
             QuandEntreZoo = DateTime.Now;
             Position = position;
             //Image = random entre les 4
+            Nom = DeterminerNomComplet(SexeVisiteur);
+        }
+
+        private string DeterminerNomComplet(SexeEntite sexeVisiteur)
+        {
+            return DeterminerPrenom(sexeVisiteur) + " " + DeterminerNom();
+        }
+
+        private string DeterminerPrenom(SexeEntite sexeVisiteur)
+        {
+            switch (sexeVisiteur)
+            {
+                case SexeEntite.F:
+                    return _prenoms[_r.Next(0, 15)];
+                case SexeEntite.M:
+                    return _prenoms[_r.Next(15, 30)];
+            }
+            return "";
+        }
+        
+        private string DeterminerNom()
+        {
+            return _noms[_r.Next(0, 15)];
         }
     }
 
